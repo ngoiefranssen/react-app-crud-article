3372a8e1bb851dc2e6ecf54fdaad8f3f8415e6fe
Franssen <ngoiefranssen04@gmail.com>
2022-11-24 15:59:18 +0200

    Wip

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Navbar() {
   return (
     <div className='container'>
-      <nav className="navbar navbar-expand-lg  bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -12,7 +12,7 @@ export default function Navbar() {
                   aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-    
+          <button className='btu btn-outline'></button>
         </div>
       </nav>
     </div>
