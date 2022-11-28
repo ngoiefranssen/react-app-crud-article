e44ebf40e6edf5b590525be8988806e0e6a04af8
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 10:44:15 +0200

    Wip

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,18 +3,17 @@ import React, { useEffect, useState } from 'react'
 
 export default function Home() {
 
-    const [users, setUsers] = useState([]);
+    const [articles, setArticle] = useState([]);
 
     useEffect(() =>{
         api_calls();
-    },[])
+    },[]);
 
     const api_calls = async () =>{
         
-        const apiData = '';
-        const resultApiData = await axios.get(apiData);
+        const resultApiData = await axios.get('http://192.168.10.53:8088/article');
 
-        setUsers(resultApiData.data)
+        setArticle(resultApiData.data)
     }
     
     
@@ -25,24 +24,24 @@ export default function Home() {
                 <thead>
                     <tr>
                         <th scope='col'>#</th>
-                        <th scope='col'>Name</th>
-                        <th scope='col'>User name</th>
-                        <th scope='col'>Email</th>
+                        <th scope='col'>Name article</th>
+                        <th scope='col'>Description</th>
+                        {/* <th scope='col'>Email</th> */}
                         <th scope='col'>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
-                        users.users?.map((user, index) => (
+                        articles.users?.map((article, index) => (
                             <tr>
-                                <th scope='row' key={index}>{index+1}</th>
-                                <td>{}</td>
-                                <td>{}</td>
-                                <td>{}</td>
+                                <th scope='row' key={index.idarticle}>{index+1}</th>
+                                <td>{article.nomarticle}</td>
+                                <td>{article.description}</td>
+                                {/* <td>{}</td> */}
                                 <td>
-                                    <button className='btn btn'></button>
-                                    <button className='btn btn'></button>
-                                    <button className='btn btn'></button>
+                                    <button className='btn btn-primary mx-2'>View</button>
+                                    <button className='btn btn-outline-success mx-2'>Edit</button>
+                                    <button className='btn btn-danger mx-2'>Delete</button>
                                 </td>
                             </tr>
                         ))
