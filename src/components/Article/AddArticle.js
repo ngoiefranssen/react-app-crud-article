58aa54a07b467538f331642070a74d4efb96b65f
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 15:32:46 +0200

    Wip

--- src/components/Article/AddArticle.js
+++ src/components/Article/AddArticle.js
@@ -1,6 +1,10 @@
-import React,  {useState} from 'react';
+import axios from 'axios';
+import React,  { useState} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function AddArticle() {
+
+  const navigate = useNavigate();
 
   const [article, setArticle] = useState({
 
@@ -8,14 +12,23 @@ export default function AddArticle() {
     description : ""
   });
 
-  const {nomarticle, description} =article;
+  const {nomarticle, description} = article;
 
   const onInputChange = (e) =>{
 
     e.preventDefault()
 
-    setArticle({ ...article, [e.target.nomarticle] : e.target.value});
+    setArticle({ ...article, [e.target.nomarticle] : e.target.value });
   }
+
+  const onsubmit = async () =>{
+
+    await axios.post('http://192.168.10.53:8088/article', article);
+    navigate('/');
+  };
+
+
+
   return (
     <div className='container'>
       <div className='row'>
@@ -23,12 +36,16 @@ export default function AddArticle() {
           <h2 className='text-center m-4'>Register Article</h2>
           <div  className='mt-3'>
             <input  type={"text"} className="form-control"
-                    placeholder='Enter your name...........' name='name'/>
+                    placeholder='Enter your name...........' name='name'
+                    value={nomarticle}/>
+                    onChange={(e) => onInputChange(e)}
           </div>
           <div  className='mt-3'>
           <textarea className="form-control"  type={"text"} rows="3"
                     placeholder="Message sent to description..."
-                    name='description'></textarea>
+                    name='description'
+                    value={description}
+                    onChange = {(e) => onInputChange(e)}></textarea>
           </div>
           <div className='text-center'>
             <button type='submit' className='btn btn-outline-primary my-3 mx-4'>
