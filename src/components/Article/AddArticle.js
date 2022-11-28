2cb34e0fc263f3cdefb90dc7d9d9153ce279bfc9
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 16:00:53 +0200

    Wip

--- src/components/Article/AddArticle.js
+++ src/components/Article/AddArticle.js
@@ -21,7 +21,7 @@ export default function AddArticle() {
     setArticle({ ...article, [e.target.nomarticle] : e.target.value });
   }
 
-  const onsubmit = async () =>{
+  const onSubmit = async (e) =>{
 
     await axios.post('http://192.168.10.53:8088/article', article);
     navigate('/');
@@ -34,14 +34,16 @@ export default function AddArticle() {
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Register Article</h2>
+          <form onSubmit={(e) => onSubmit(e)}>
           <div  className='mt-3'>
-            <input  type={"text"} className="form-control"
-                    placeholder='Enter your name...........' name='name'
-                    value={nomarticle}/>
+            <input  type="text" className="form-control"
+                    placeholder='Enter your name...........' name='nomarticle'
+                    value={nomarticle}
                     onChange={(e) => onInputChange(e)}
+                    />
           </div>
           <div  className='mt-3'>
-          <textarea className="form-control"  type={"text"} rows="3"
+          <textarea className="form-control"  type="text" rows="3"
                     placeholder="Message sent to description..."
                     name='description'
                     value={description}
@@ -55,6 +57,7 @@ export default function AddArticle() {
               Cancel
             </Link>
           </div>
+          </form>
         </div>
       </div>
     </div>
