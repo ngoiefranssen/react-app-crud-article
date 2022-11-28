106d62e62ec3e161aeb91a21268b1db6f6669462
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 16:06:42 +0200

    Wip

--- src/components/Article/EditArticle.js
+++ src/components/Article/EditArticle.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 import React,  { useState} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
-export default function AddArticle() {
+export default function EditArticle() {
 
   let navigate = useNavigate();
 
   const [article, setArticle] = useState({
 
     nomarticle : "",
-    description : ""
+    description : "",
   });
 
   const {nomarticle, description} = article;
@@ -27,13 +27,11 @@ export default function AddArticle() {
     navigate('/');
   };
 
-
-
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
-          <h2 className='text-center m-4'>Register Article</h2>
+          <h2 className='text-center m-4'> Article</h2>
           <form onSubmit={(e) => onSubmit(e)}>
           <div  className='mt-3'>
             <input  type="text" className="form-control"
