b86ea1c13d46d503b8000455a1aa64507ac5f9ce
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 11:35:48 +0200

    Wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Navbar from "./components/layouts/Navbar";
 import Home from "./components/pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import userAdd from "./components/users/userAdd";
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      < Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/useradd' element={<userAdd />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
