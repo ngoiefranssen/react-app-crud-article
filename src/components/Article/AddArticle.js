3e5fe25a53960222981c32503c15467b8497ff73
Franssen <ngoiefranssen04@gmail.com>
2022-11-28 14:14:15 +0200

    Wip

--- src/components/Article/AddArticle.js
+++ src/components/Article/AddArticle.js
@@ -6,7 +6,11 @@ export default function AddArticle() {
       <div className='row'>
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Register Article</h2>
-          <div className='mt-3'>
+          <div  className='mt-3'>
+            <input  type={"text"} className="form-control"
+                    placeholder='Enter you name..........' name='name'/>
+          </div>
+          <div  className='mt-3'>
             <input  type={"text"} className="form-control"
                     placeholder='Enter you name..........' name='name'/>
           </div>
