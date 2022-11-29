4bba88cd9b276c50b3dcc4929e2bf964f07a61bf
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 08:21:58 +0200

    Wip

--- src/components/Article/ViewArticle.js
+++ src/components/Article/ViewArticle.js
@@ -7,7 +7,7 @@ export default function ViewArticle() {
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Register Article</h2>
           <div className='card'>
-
+            
           </div>
         </div>
       </div>
