761597a5317e15b2eea191195006dc0d8d5e312e
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 09:09:38 +0200

    Wip

--- src/components/Article/ViewArticle.js
+++ src/components/Article/ViewArticle.js
@@ -1,12 +1,23 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ViewArticle() {
 
   const [articleView, setArticleView] = useState({
 
-    
-  }, [])
+    nomarticle,
+    description,
+  });
+
+  useEffect(() => {
+    loadDataView();
+  })
+
+  const loadDataView = async () =>{
+    const resultViewData = await axios.get(`http://192.168.10.53:8088/article`);
+    setArticleView(resultViewData.data);
+  }
   return (
     <div className='container'>
       <div className='row'>
