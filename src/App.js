84648f94a28c8868366dd083ead641f3b7fb9e84
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 09:30:15 +0200

    Wip

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AddArticle from "./components/Article/AddArticle";
 import EditArticle from "./components/Article/EditArticle";
-import ViewArticle from "./components/Article/ViewArticle";
+// import ViewArticle from "./components/Article/ViewArticle";
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           <Route path='/' element={<Home />}/>
           <Route path='/add_article' element={<AddArticle />} />
           <Route path='/editarticle/:idarticle' element={<EditArticle />} />
-          <Route path='/viewarticle/:idarticle' element={< ViewArticle/>}/>
+          {/* <Route path='/viewarticle/:idarticle' element={< ViewArticle/>}/> */}
         </Routes>
       </Router>
     </div>
