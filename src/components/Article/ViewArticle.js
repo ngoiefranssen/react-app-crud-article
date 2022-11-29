41e8a529a0e663906383390d3252739ffb708ac9
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 08:33:30 +0200

    Wip

--- src/components/Article/ViewArticle.js
+++ src/components/Article/ViewArticle.js
@@ -7,7 +7,10 @@ export default function ViewArticle() {
         <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
           <h2 className='text-center m-4'>Article Details</h2>
           <div className='card'>
-            
+             <div className='card-header'>
+              Details for Article id :
+              <ul className=''></ul> 
+             </div>
           </div>
         </div>
       </div>
