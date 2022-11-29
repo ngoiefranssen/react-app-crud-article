9c985eb807bf6531ec1bfe8007d70acc4c7a6774
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 09:04:22 +0200

    Wip

--- src/components/Article/ViewArticle.js
+++ src/components/Article/ViewArticle.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function ViewArticle() {
+
+  const [articleView, setArticleView] = useState({
+    
+  })
   return (
     <div className='container'>
       <div className='row'>
