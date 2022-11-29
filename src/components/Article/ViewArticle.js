12253a1c1a48ae5b3c11ec6579a248cd44b192c1
Franssen <ngoiefranssen04@gmail.com>
2022-11-29 07:57:59 +0200

    Wip

---
+++ src/components/Article/ViewArticle.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function ViewArticle() {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 border rounded p-4 mt-2 shadow'>
+          <h2 className='text-center m-4'>Register Article</h2>
+          <div className='card'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
